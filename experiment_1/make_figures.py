e6c3ce46789bd0c869e042fc75fdf6cb936c310a
xuehuanran <935254102@qq.com>
2017-04-16 21:37:46 +0800

    modify epoch to entire data set

--- experiment_1/make_figures.py
+++ experiment_1/make_figures.py
@@ -4,9 +4,20 @@ import numpy as np
 loss0 = np.load('output/loss0.npy')
 loss1 = np.load('output/loss1.npy')
 
+data0 = []
+data1 = []
+
+for i in range(0, 1100, 11):
+    data0.append(loss0[i])
+    data1.append(loss1[i])
+
+x = []
+for i in range(0, 1100, 11):
+    x.append(i / 1100)
+
 plt.figure(1)
-curve0, = plt.semilogy(loss0, 'r')
-curve1, = plt.semilogy(loss1, 'b')
+curve0, = plt.semilogy(x, data0, 'r')
+curve1, = plt.semilogy(x, data1, 'b')
 
 plt.xlabel('epoch')
 plt.ylabel('loss')
